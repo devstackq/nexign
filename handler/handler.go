25a0585fd4d3f43a23f5443f080188b0d64c04a4
devstackq <loginpsn@mail.ru>
2022-06-29 21:21:05 +0600

    refactor, another way

--- handler/handler.go
+++ handler/handler.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"strings"
 
 	"github.com/devstackq/nexign/config"
 	"github.com/devstackq/nexign/entity"
@@ -26,7 +26,8 @@ type handler struct {
 // }
 
 type textgears struct {
-	Text []*string `json:"texts"`
+	Sentences []string `json:"texts"`
+	text      *string
 }
 
 func New(lg *zap.Logger, cfg *config.Config) *handler {
@@ -44,41 +45,80 @@ func (c *handler) hSpeller(g *gin.Context) {
 		return
 	}
 
-	tg.concurrentRequest(c.cfg)
+	// tg.concurrentRequest(c.cfg)
+	tg.convertToStr()
+	if err = tg.spelling(c.cfg); err != nil {
+		c.lg.Info(err.Error())
+		g.AbortWithStatusJSON(http.StatusBadRequest, nil)
+		return
+	}
 
-	g.JSON(http.StatusOK, tg.Text)
+	g.JSON(http.StatusOK, tg.Sentences)
 }
 
-func (s *textgears) spelling(cfg *config.Config, sentence *string, result chan entity.Result, errCh chan error) {
+func (tg *textgears) spelling(cfg *config.Config) error {
 	var (
 		response = entity.Result{}
 		url      string
 		err      error
 	)
-	url = fmt.Sprint(cfg.SpellerCfg.Url, "/spelling?key=", cfg.Key, "&language=", cfg.Language, "&text=", *sentence)
+	url = fmt.Sprint(cfg.SpellerCfg.Url, "/spelling?key=", cfg.Key, "&language=", cfg.Language, "&text=", *tg.text)
 
 	resp, err := http.Post(url, "application/json", nil)
 	if err != nil {
-		errCh <- err
-		log.Println(err)
-		return
+		return err
 	}
 	bb, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		errCh <- err
-		log.Println(err)
-		return
+		return err
 	}
 
 	err = json.Unmarshal(bb, &response)
 	if err != nil {
-		errCh <- err
-		log.Println(err)
-		return
+		return err
 	}
-	errCh <- nil
-	result <- response
+	temp := ""
+
+	if len(response.Errors) > 0 {
+		for _, word := range response.Errors {
+			temp = strings.Replace(*tg.text, word.Bad, word.Better[0], -1)
+			tg.text = &temp
+		}
+	}
+
+	tempList := strings.Split(*tg.text, "|")
+	tg.Sentences = tempList
+	return nil
 }
+
+// func (s *textgears) spelling(cfg *config.Config, sentence *string, result chan entity.Result, errCh chan error) {
+// 	var (
+// 		response = entity.Result{}
+// 		url      string
+// 		err      error
+// 	)
+// 	url = fmt.Sprint(cfg.SpellerCfg.Url, "/spelling?key=", cfg.Key, "&language=", cfg.Language, "&text=", *sentence)
+
+// 	resp, err := http.Post(url, "application/json", nil)
+// 	if err != nil {
+// 		errCh <- err
+// 		return
+// 	}
+// 	bb, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		errCh <- err
+// 		return
+// 	}
+
+// 	err = json.Unmarshal(bb, &response)
+// 	if err != nil {
+// 		errCh <- err
+// 		return
+// 	}
+
+// 	errCh <- nil
+// 	result <- response
+// }
 
 // register handler
 func (c *handler) Route() http.Handler {
--- handler/utils.go
+++ handler/utils.go
@@ -1,42 +1,41 @@
 package handler
 
-import (
-	"strings"
-	"sync"
-
-	"github.com/devstackq/nexign/config"
-	"github.com/devstackq/nexign/entity"
-)
-
-func (tg *textgears) concurrentRequest(cfg *config.Config) {
-	var (
-		wg     sync.WaitGroup
-		result = make(chan entity.Result, 1)
-		errCh  = make(chan error, 1)
-	)
-
-	for idxSent, sentence := range tg.Text {
-		wg.Add(1)
-		go tg.spelling(cfg, sentence, result, errCh)
-
-		tempRes := <-result
-
-		if <-errCh == nil {
-			// var wg2 sync.WaitGroup
-			// go func(tempRes entity.Result, idxSent int) {
-			if len(tempRes.Errors) > 0 {
-				// wg2.Add(1)
-				f := ""
-				for _, word := range tempRes.Errors {
-					f = strings.Replace(*tg.Text[idxSent], word.Bad, word.Better[0], -1)
-					tg.Text[idxSent] = &f
-				}
-				// wg2.Done()
-			}
-			// }(<-result, idxSent)
-			// wg2.Wait()
-		}
-		wg.Done()
+func (tg *textgears) convertToStr() {
+	str := ""
+	for _, text := range tg.Sentences {
+		str += text + "|"
 	}
-	wg.Wait()
+	tg.text = &str
 }
+
+// func (tg *textgears) concurrentRequest(cfg *config.Config) {
+// 	var (
+// 		wg     sync.WaitGroup
+// 		result = make(chan entity.Result, 1)
+// 		errCh  = make(chan error, 1)
+// 	)
+// 	for idxSent, sentence := range tg.Sentences {
+// 		wg.Add(1)
+// 		go tg.spelling(cfg, sentence, result, errCh)
+
+// 		tempRes := <-result
+
+// 		if <-errCh == nil {
+// 			// var wg2 sync.WaitGroup
+// 			// go func(tempRes entity.Result, idxSent int) {
+// 			if len(tempRes.Errors) > 0 {
+// 				// wg2.Add(1)
+// 				f := ""
+// 				for _, word := range tempRes.Errors {
+// 					f = strings.Replace(*tg.Sentences[idxSent], word.Bad, word.Better[0], -1)
+// 					tg.Sentences[idxSent] = &f
+// 				}
+// 				// wg2.Done()
+// 			}
+// 			// }(<-result, idxSent)
+// 			// wg2.Wait()
+// 		}
+// 		wg.Done()
+// 	}
+// 	wg.Wait()
+// }
